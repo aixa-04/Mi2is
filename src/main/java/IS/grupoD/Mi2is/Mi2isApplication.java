61bc9901411c38ee5fb03ad7e1d144b9a9e449c5
aixa-04 <aixahamseg4@uma.es>
2025-11-20 10:17:42 +0100

    Creación del proyecto

---
+++ src/main/java/IS/grupoD/Mi2is/Mi2isApplication.java
@@ -0,0 +1,13 @@
+package IS.grupoD.Mi2is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mi2isApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mi2isApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IS/grupoD/Mi2is/Mi2isApplicationTests.java
@@ -0,0 +1,13 @@
+package IS.grupoD.Mi2is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mi2isApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
