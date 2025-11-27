4bbe44fcdc426ec1a82a446d2fa657e908f138bd
Lau-499 <laurasanchezw@gmail.com>
2025-11-27 10:02:43 +0100

    Entidad: usuario

--- Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/Usuario.java
+++ Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/Usuario.java
@@ -1,1 +1,41 @@
+package IS.grupoD.Mi2is.P6;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+
+    private String nombre;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    private String apellidos;
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+
+    
+
+}
