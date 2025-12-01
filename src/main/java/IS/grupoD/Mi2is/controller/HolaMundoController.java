3b8e5b65bd4ccb3e3c9b5425adef7cc1ee2dce76
Lau-499 <laurasanchezw@gmail.com>
2025-12-01 11:04:49 +0100

    No he hecho nada

--- src/main/java/IS/grupoD/Mi2is/controller/HolaMundoController.java
+++ src/main/java/IS/grupoD/Mi2is/controller/HolaMundoController.java
@@ -1,9 +1,11 @@
 package IS.grupoD.Mi2is.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HolaMundoController {
 
     @GetMapping("/hola")
