35bada27e970bb15ee1ef311494a18ed59fa6899
Lau-499 <laurasanchezw@gmail.com>
2025-12-01 11:51:41 +0100

    Cambios que pidió Belén

--- src/main/java/IS/grupoD/Mi2is/controller/HolaMundoController.java
+++ src/main/java/IS/grupoD/Mi2is/controller/HolaMundoController.java
@@ -10,7 +10,7 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String holaMundo() {
-        return "¡Hola Mundo!";
+        return "¡Hola Mundo! Esto lo hicimos el sábado 29/11/2025.";
     }
 
     @GetMapping("/")
